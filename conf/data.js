afe09e40ac06f46dac0e5dc35ad88f2b262376a8
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-01-06 15:58:57 +0000

    adding some test data

--- conf/data.js
+++ conf/data.js
@@ -1,4 +1,11 @@
 db.getCollection('vehicles').save([
     {reg: "NA08MYW", make: "Ford", model: "Focus", year: 2008, color: "Silver"},
-    {reg: "ST1NK3Y", make: "Citroen", model: "C3", year: 2003, color: "Red"}
+    {reg: "ST1NK3Y", make: "Citroen", model: "C3", year: 2003, color: "Red"},
+    {reg: "F00F00", make: "Skoda", model: "Superb", year:  2000, color: "Green" },
+    {reg: "B00B13S", make: "Skoda", model: "Superb", year: 2009, color: "Blue" },
+    {reg: "NA08MYX", make: "Ford", model: "Mustang", year: 2014, color: "Cyan" },
+    {reg: "P00POO", make: "Skoda", model: "Superb", year: 2005, color: "Brown" },
+    {reg: "N0C010R", make: "Skoda", model: "Superb", year: 2005 }
 ])
+
+
