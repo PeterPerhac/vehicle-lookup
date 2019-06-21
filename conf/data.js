ae4ccdae6d97ce8c5c70d1c698cd7c91221bcf13
Peter Perhac <perhac.peter@gmail.com>
2019-06-21 22:24:52 +0100

    added zafira

--- conf/data.js
+++ conf/data.js
@@ -1,5 +1,6 @@
 db.getCollection('vehicles').save([
     {reg: "NA08MYW", make: "Ford", model: "Focus", year: 2008, color: "Silver"},
+    {reg: "HY13VLV", make: "Vauxhall", model: "Zafira Tourer", year: 2013, color: "Red"},
     {reg: "ST1NK3Y", make: "Citroen", model: "C3", year: 2003, color: "Red"},
     {reg: "F00F00", make: "Skoda", model: "Superb", year:  2000, color: "Green" },
     {reg: "B00B13S", make: "Skoda", model: "Superb", year: 2009, color: "Blue" },
